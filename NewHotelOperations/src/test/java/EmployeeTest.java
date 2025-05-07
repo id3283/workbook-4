e3659ab8a7bce05896cea2e595490a94333e0a02
Dave <id3283@users.noreply.github.com>
2025-05-07 18:14:15 +0100

    Added note explaining test instance lifecycle default.

--- NewHotelOperations/src/test/java/EmployeeTest.java
+++ NewHotelOperations/src/test/java/EmployeeTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)   // the default, so you rarely see it written out
 class EmployeeTest {
     Employee employee = new Employee();
 
